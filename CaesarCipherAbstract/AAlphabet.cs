165cae9182904d74f18b754ce2b047f880181162
May Lee (InConsulting Inc.) <v-maylee@microsoft.com>
2016-07-23 15:50:23 -0700

    Add abstract class

--- CaesarCipherAbstract/AAlphabet.cs
+++ CaesarCipherAbstract/AAlphabet.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace CaesarCipherAbstract
 {
-    abstract AAlphabet
+    //can also be protected
+    public abstract class AAlphabet
     {
+
+        #region Properties
+        public char[] Alphabet { get; set; }
+        #endregion
+
+        /// <summary>
+        /// When abstract class is implemented it will require as parameter
+        /// the alphabet.
+        /// 
+        /// </summary>
+        /// <param name="alphabet">Array of chars</param>
+        #region Constructor
+        public AAlphabet(char[] alphabet)
+        {
+            this.Alphabet = alphabet;
+        }
+
+        #endregion
+
+        #region Methods
+        public abstract void Transpose(int offset);
+        public abstract char GetTransposedChar(char c);
+        #endregion
     }
 }
