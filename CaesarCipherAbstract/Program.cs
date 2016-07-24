6023c9c532af8d20d5959a2db2f8c99b9d952a95
May Lee (InConsulting Inc.) <v-maylee@microsoft.com>
2016-07-23 17:24:29 -0700

    Test DictionaryBasedAlphabet to cipher text

--- CaesarCipherAbstract/Program.cs
+++ CaesarCipherAbstract/Program.cs
@@ -10,9 +10,24 @@ namespace CaesarCipherAbstract
     {
         static void Main(string[] args)
         {
+            string text = "my name is may";
+            Console.WriteLine($"Original: {text}");
             string alphaString = "abcdefghijklmnopqrstuvwxyz ";
             char[] alphaArray = alphaString.ToCharArray();
+
             DictionaryBasedAlphabet dictionaryAlpha = new DictionaryBasedAlphabet(alphaArray);
+            CaesarCipher Ciphered = new CaesarCipher(dictionaryAlpha);
+            string cipheredText = Ciphered.Cipher(text, 3);
+            Console.WriteLine($"Ciphered:  {cipheredText}");
+
+            string decipherAlpha = "defghijklmnopqrstuvwxyz abc";
+            char[] decipherArray = decipherAlpha.ToCharArray();
+            DictionaryBasedAlphabet deciferAlpha = new DictionaryBasedAlphabet(decipherArray);
+            CaesarCipher Deciphered = new CaesarCipher(deciferAlpha);
+            string decipheredText = Deciphered.Cipher(cipheredText, -3);
+            Console.WriteLine($"Deciphered:  {decipheredText}");
+
+
             //CaesarCipher caeserCipher = new CaesarCipher("abcdefghijklmnopqrstuvwxyz ");
             //caeserCipher.SetOffset();
             //string text = "my name is may";
